cd874a87934edfff42ed505b33b1a15d9b54d41d
Onecold <wyh19990727@gmail.com>
2023-06-15 14:43:02 -0400

    Update every 10 secs and label a new marker on the map

--- travel_app/src/Map.js
+++ travel_app/src/Map.js
@@ -1,35 +1,44 @@
 import { useState, useEffect } from 'react';
-import {GoogleMap, useLoadScript, MarkerF} from '@react-google-maps/api';
+import { GoogleMap, useLoadScript, MarkerF } from '@react-google-maps/api';
 import './App.css';
 
 export default function MapFragment() {
-    const [currentPosition, setCurrentPosition] = useState(null);
+  const [positions, setPositions] = useState([]);
 
-    const { isLoaded } = useLoadScript({
-        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    });
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  });
 
-    useEffect(() => {
-      // Get the user's current geolocation coordinates
+  useEffect(() => {
+    // Get the user's current geolocation coordinates
+    const interval = setInterval(() => {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const { latitude, longitude } = position.coords;
-          setCurrentPosition({ lat: latitude, lng: longitude });
+          setPositions((prev) => [...prev, { lat: latitude, lng: longitude }]);
         },
         (error) => {
           console.error('Error retrieving geolocation:', error);
         }
       );
-    }, []);
+    }, 10000);
 
-    if (!isLoaded) return <div>Loading..</div>
-    return (
-        <GoogleMap 
-            zoom={14} 
-            center={currentPosition} 
-            mapContainerClassName="map-container"
-        >
-          <MarkerF position={currentPosition} />
-        </GoogleMap>
-    );
+    // Cleanup function to clear interval when component unmounts
+    return () => clearInterval(interval);
+  }, []);
+
+  if (!isLoaded) return <div>Loading..</div>
+
+  return (
+    <GoogleMap
+      zoom={14}
+      center={positions[positions.length - 1]}
+      mapContainerClassName="map-container"
+    >
+      {positions.map((position, index) => (
+        <MarkerF key={index} position={position} />
+      ))}
+
+    </GoogleMap>
+  );
 }
