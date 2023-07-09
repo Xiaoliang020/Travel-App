cdd936a14a91c9cfa8a6734e5ab395fe4510aa5d
Xiaoliang Chen <70415185+Xiaoliang020@users.noreply.github.com>
2023-07-09 01:44:03 -0400

    Update build files

---
+++ travel_app/build/static/js/304.2e8a375d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktravel_app=self.webpackChunktravel_app||[]).push([[304],{304:function(e,n,s){s.r(n);var a=s(184);n.default=function(){return(0,a.jsx)("div",{className:"community",children:(0,a.jsx)("p",{children:"This is Community"})})}}}]);
+//# sourceMappingURL=304.2e8a375d.chunk.js.map
