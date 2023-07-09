a25a66564065d06237aa78ae3d98d43497ce8341
Xiaoliang Chen <70415185+Xiaoliang020@users.noreply.github.com>
2023-07-09 17:36:50 -0400

    Add dark mode in map.js without using map id

--- travel_app/src/views/Map.js
+++ travel_app/src/views/Map.js
@@ -30,6 +30,31 @@ export default function Map() {
   const mapId = theme === 'default' ? '17e23ad9dc98cd76' : '965d3fbc319fcf57';
 
   const [isTakingScreenshot, setIsTakingScreenshot] = useState(false);
+  
+  const darkMode = {
+    fullscreenControl: false,
+    streetViewControl: false,
+    styles: [
+      { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+      { featureType: "administrative.locality", elementType: "labels.text.fill", stylers: [{ color: "#d59563" }] },
+      { featureType: "poi", elementType: "labels.text.fill", stylers: [{ color: "#d59563" }] },
+      { featureType: "poi.park", elementType: "geometry", stylers: [{ color: "#263c3f" }] },
+      { featureType: "poi.park", elementType: "labels.text.fill", stylers: [{ color: "#6b9a76" }] },
+      { featureType: "road", elementType: "geometry", stylers: [{ color: "#38414e" }] },
+      { featureType: "road", elementType: "geometry.stroke", stylers: [{ color: "#212a37" }] },
+      { featureType: "road", elementType: "labels.text.fill", stylers: [{ color: "#9ca5b3" }] },
+      { featureType: "road.highway", elementType: "geometry", stylers: [{ color: "#746855" }] },
+      { featureType: "road.highway", elementType: "geometry.stroke", stylers: [{ color: "#1f2835" }] },
+      { featureType: "road.highway", elementType: "labels.text.fill", stylers: [{ color: "#f3d19c" }] },
+      { featureType: "transit", elementType: "geometry", stylers: [{ color: "#2f3948" }] },
+      { featureType: "transit.station", elementType: "labels.text.fill", stylers: [{ color: "#d59563" }] },
+      { featureType: "water", elementType: "geometry", stylers: [{ color: "#17263c" }] },
+      { featureType: "water", elementType: "labels.text.fill", stylers: [{ color: "#515c6d" }] },
+      { featureType: "water", elementType: "labels.text.stroke", stylers: [{ color: "#17263c" }] },
+    ],
+  };
 
   // Get the user's current location
   useEffect(() => {
@@ -203,11 +228,12 @@ export default function Map() {
           zoom={mapZoom}
           mapContainerClassName="map-container"
           // Problem: Cannot show the map's detail in the screenshot when enable mapId
-          options={{
-            mapId: mapId,
-            fullscreenControl: false, // 添加这一行来隐藏全屏控件
-            streetViewControl: false,
-          }}
+          // options={{
+          //   mapId: mapId,
+          //   fullscreenControl: false, // 添加这一行来隐藏全屏控件
+          //   streetViewControl: false,
+          // }}
+          options={theme === 'default' ? {} : darkMode}
         >
           {isPathsVisible && Object.values(positionByPathId).map((pathPositions, index) => (
             <PolylineF
