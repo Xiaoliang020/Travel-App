71411303678e2e6befb4add1ad684f407e32c83e
Xiaoliang Chen <70415185+Xiaoliang020@users.noreply.github.com>
2023-07-11 14:58:44 -0400

    Add simple data interaction between frontend and backend

--- back-end/src/main/java/com/travelapp/controller/HelloController.java
+++ back-end/src/main/java/com/travelapp/controller/HelloController.java
@@ -1,5 +1,9 @@
 package com.travelapp.controller;
 
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +12,12 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello, World!";
+        return "Hello, Frontend!";
+    }
+
+    @PostMapping("/print")
+    public void printMessage(@RequestBody Map<String, String> requestBody) {
+        String message = requestBody.get("message");
+        System.out.println("Received message from frontend: " + message);
     }
 }
--- travel_app/src/views/HelloWorld.js
+++ travel_app/src/views/HelloWorld.js
@@ -10,5 +10,30 @@ export default function HelloWorld() {
       .catch(error => console.error('Error:', error));
   }, []);
 
-  return <div>{message}</div>;
+  const handleClick = async () => {
+    try {
+      const response = await fetch('/print', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message: 'Hello, Backend' }),
+      });
+
+      if (response.ok) {
+        console.log('Message sent to backend successfully');
+      } else {
+        console.log('Failed to send message to backend');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div>
+      <div>{message}</div>
+      <button onClick={handleClick}>Send Message to Backend</button>
+    </div>
+  );
 }
