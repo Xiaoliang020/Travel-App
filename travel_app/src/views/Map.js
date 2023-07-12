a51761ce10c5bf94b51944c1e210ed85b88c5121
YongzhiGao <gaoyongzhi0601@163.com>
2023-07-12 00:46:05 -0400

    Interactive Marker
    new version with picture and word when adding new marker

--- travel_app/src/views/Map.js
+++ travel_app/src/views/Map.js
@@ -8,7 +8,6 @@ import { ThemeContext } from '../App';
 import { PlayCircleOutlined, StopOutlined, CompassOutlined, EyeOutlined, EyeInvisibleOutlined, PlusOutlined, ClearOutlined, DownloadOutlined, ShareAltOutlined } from '@ant-design/icons';
 import html2canvas from 'html2canvas';
 import Geocode from "react-geocode";
-import { UploadOutlined } from '@ant-design/icons';
 import { LoadingOutlined} from '@ant-design/icons';
 
 export default function Map() {
@@ -31,6 +30,30 @@ export default function Map() {
 
   const { addPath, displayedPath, setDisplayedPath } = useContext(SavedPathsContext);
   const { theme } = useContext(ThemeContext);
+
+  const getBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
+  };
+
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [previewImage, setPreviewImage] = useState("");
+  const [previewTitle, setPreviewTitle] = useState("");
+
+  const handleCancel = () => setPreviewOpen(false);
+
+  const handlePreview = async (file) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+  
+    setPreviewImage(file.url || file.preview);
+    setPreviewOpen(true);
+  };
 
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
@@ -72,18 +95,6 @@ export default function Map() {
       <div style={{ marginTop: 8 }}>Upload</div>
     </div>
   );
-  
-  const beforeUpload = (file) => {
-    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-    if (!isJpgOrPng) {
-      message.error('You can only upload JPG/PNG file!');
-    }
-    const isLt2M = file.size / 1024 / 1024 < 2;
-    if (!isLt2M) {
-      message.error('Image must smaller than 2MB!');
-    }
-    return isJpgOrPng && isLt2M;
-  };
   
   const handleChange = (info) => {
     if (info.file.status === 'uploading') {
@@ -286,7 +297,7 @@ export default function Map() {
 
   const handleMarkerClick = () => {
     Modal.confirm({
-      title: 'Input Something',
+      title: 'The comment you left in this place',
       content: (
         <Input
           placeholder="Input something..."
@@ -325,24 +336,34 @@ export default function Map() {
       title: 'Set a New Marker',
       content: (
         <div>
+          Marker icon:
+          <Upload
+            listType="picture-circle"
+            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+            onPreview={handlePreview}
+            onChange={handleChange}
+          >
+            {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+         
+          </Upload>
+
+          Comment:
+          
           <Input
           placeholder="Input something..."
           defaultValue={inputText}
           onChange={(e) => setInputText(e.target.value)}
         />
 
-          <Upload
-            name="avatar"
-            listType="picture-card"
-            className="avatar-uploader"
-            showUploadList={false}
-            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-            beforeUpload={beforeUpload}
-            onChange={handleChange}
+
+          <Modal 
+            open = {previewOpen} 
+            title = 'The Marker Preview'
+            footer = {null}
+            onCancel ={handleCancel}
           >
-            {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
-          
-          </Upload>
+            <img alt="example" style={{ width: '100%' }} src={previewImage} />
+          </Modal>
         </div>
       ),
       onOk: (close) => {
