7b9f9302e8448a5daef498668bcfe8c1688b510f
Xiaoliang Chen <70415185+Xiaoliang020@users.noreply.github.com>
2023-07-26 21:47:22 -0400

    Add cors config at backend

---
+++ back-end/src/main/java/com/travelapp/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.travelapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://34.162.232.130:8000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
