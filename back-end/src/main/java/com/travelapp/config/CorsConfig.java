b0ed853e01898ba3956a482842a83d60cccf8ac0
Xiaoliang Chen <70415185+Xiaoliang020@users.noreply.github.com>
2023-07-27 16:49:54 -0400

    Change cors config at backend

--- back-end/src/main/java/com/travelapp/config/CorsConfig.java
+++ back-end/src/main/java/com/travelapp/config/CorsConfig.java
@@ -7,14 +7,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+    private static final int MAXAGE = 3600;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://34.162.232.130:8000")
+                .allowedOrigins("https://simple-travel-app.netlify.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
-                .maxAge(3600);
+                .maxAge(MAXAGE);
     }
 }
